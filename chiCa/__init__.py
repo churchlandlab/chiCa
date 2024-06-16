ffa62ce188ecf755ab023fdf613f8ccfe891d798
Gabriel Rojas Bowe <grojasbowe@gmail.com>
2024-06-16 13:08:32 -0700

    So many stupid errors when committing. It's because I'm doing this very casually. Sorry Lukas.

---
+++ chiCa/__init__.py
@@ -0,0 +1,9 @@
+"""
+
+chiCa: a package for analyzing behavioral and miniscope data from chipmunk
+
+"""
+from .chipmunk_analysis_tools import *
+from .decoding_utils import *
+from .encoding_utils import *
+from .visualization_utils import *
