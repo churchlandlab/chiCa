bff2fe915751cc30488fa981cbf2e80be99cd50a
Gabriel Rojas Bowe <grojasbowe@gmail.com>
2024-06-16 13:02:06 -0700

    Fix init file... again, potentially for the last time

--- __init__.py
+++
@@ -1,9 +0,0 @@
-"""
-
-chiCa: a package for analyzing behavioral and miniscope data from chipmunk
-
-"""
-from chiCa.chipmunk_analysis_tools import *
-from chiCa.decoding_utils import *
-from chiCa.encoding_utils import *
-from chiCa.visualization_utils import *
