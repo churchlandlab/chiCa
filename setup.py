38c6d2d7383fe8fe6f7e399a33c21d58e1f3c936
Gabriel Rojas Bowe <grojasbowe@gmail.com>
2024-06-16 12:49:04 -0700

    Init commit to make chiCa pip installable

---
+++ setup.py
@@ -0,0 +1,59 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+import os
+import sys
+from os.path import join as pjoin
+import shutil
+
+            
+description = '''chiCa: Processing of miniscope calcium imaging data from the chipmunk task.'''
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+setup(
+    name = 'chiCa',
+    version = '0.1',
+    author = 'Lukas Oesch',
+    author_email = 'LOesch@mednet.ucla.edu',
+    description = (description),
+    long_description = long_description,
+    long_description_content_type='text/markdown',
+    license = 'GPL',
+    install_requires = ['os',
+                        'scipy',
+                        'glob',
+                        'numpy',
+                        'cv2',
+                        'tkinter',
+                        'time',
+                        'pandas',
+                        'warnings',
+                        'matplotlib',
+                        'multiprocessing',
+                        'logging',
+                        'json',
+                        'sys',
+                        'sklearn',
+                        'itertools',
+                        'pathlib'], #caiman too but it's not through pip I think
+    url = "https://github.com/churchlandlab/chiCa",
+    packages = find_packages(),
+    python_requires='>=3.6'
+)
+
+if 'install' in sys.argv or 'develop' in sys.argv:
+    from labdatatools.utils import labdata_preferences
+    plugins = labdata_preferences['plugins_folder']
+
+    # if the config directory tree doesn't exist, create it
+    if not os.path.exists(plugins):
+        os.makedirs(plugins)
+
+    # copy every file from given location to the specified ``CONFIG_PATH``
+    for fname in os.listdir('analysis'):
+        fpath = os.path.join('analysis', fname)
+        if not os.path.exists(pjoin(plugins,fname)):
+            shutil.copy(fpath, plugins)
+        else:
+            print('File already exists.')
